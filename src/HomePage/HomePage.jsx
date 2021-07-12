d773dd9ad94c473f3ccf73cf17a92a91df7ac048
Yousaf Zubair <83968204+Yousafzubair991@users.noreply.github.com>
2021-07-12 12:10:19 +0500

    Add files via upload

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { userService, authenticationService } from '@/_services';
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUser: authenticationService.currentUserValue,
+            userFromApi: null
+        };
+    }
+
+    componentDidMount() {
+        const { currentUser } = this.state;
+        userService.getById(currentUser.id).then(userFromApi => this.setState({ userFromApi }));
+    }
+
+    render() {
+        const { currentUser, userFromApi } = this.state;
+        return (
+            <div>
+                
+                <h2>Welcome <strong>{currentUser.role}</strong>.</h2>
+            </div>
+        );
+    }
+}
+
+export { HomePage };
